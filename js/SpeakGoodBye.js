ded6b32647094fc9dedfbe49cfe57d83db57ce3e
shukla-04 <erasthashukla@gmail.com>
2021-05-15 01:22:32 +0530

    Small addition to our js code

--- js/SpeakGoodBye.js
+++ js/SpeakGoodBye.js
@@ -3,12 +3,17 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
-
+(function (window){
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 // var byeSpeaker =
+
+var byeSpeaker = {}
+byeSpeaker.speak = function (name){
+		console.log(speakWord + " " + name);
+	}
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
@@ -20,6 +25,9 @@ function speak(name) {
   console.log(speakWord + " " + name);
 }
 
+window.byeSpeaker = byeSpeaker;
+
+})(window);
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
--- js/SpeakHello.js
+++ js/SpeakHello.js
@@ -1,12 +1,16 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
-
+(function (window){
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 // var helloSpeaker =
-
+var helloSpeaker = {}
+helloSpeaker.speak = function (name){
+		console.log(speakWord + " " + name);
+	}
+	
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
 
@@ -17,6 +21,9 @@ function speak(name) {
   console.log(speakWord + " " + name);
 }
 
+window.helloSpeaker = helloSpeaker;
+
+})(window);
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
